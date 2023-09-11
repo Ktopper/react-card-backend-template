aaf62ca2b5a9968025895633dad6855e4854aa95
Your Name <fervorbooks@gmail.com>
2023-09-11 17:26:29 -0400

    test connect

---
+++ netlify/functions/connect.js
@@ -0,0 +1,26 @@
+
+import mysql from 'mysql2';
+
+ 
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    // Enable SSL with a basic configuration
+    rejectUnauthorized: false
+  }
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  
+  console.log('Connected to PlanetScale!');
+});
+console.log('Connected to PlanetScale!')
+connection.end()
